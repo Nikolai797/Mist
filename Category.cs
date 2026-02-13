95c2e9fd9d17a9d86a3c08598adf99588908b67d
Malikith <l.k.shishkov@gmail.com>
2026-02-13 02:54:39 +0200

    Add files via upload

---
+++ Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Data.Entities;
+
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; } = null!;
+
+    
+    public virtual ICollection<Game>? Games { get; set; }   
+}
