623d05427591dfbd596ae57c29bb981c478a520f
Malikith <l.k.shishkov@gmail.com>
2026-02-13 02:54:53 +0200

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameStore.Data.Entities;
+
+public class Game
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string Title { get; set; } = null!;
+
+    [Required]
+    [StringLength(500)]
+    public string Description { get; set; } = null!;
+
+    [Required]
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Price { get; set; }
+
+
+    public int? CategoryId { get; set; }
+
+
+    public virtual Category? Category { get; set; }
+
+    public virtual ICollection<OrderItem>? OrderItems { get; set; }
+}
