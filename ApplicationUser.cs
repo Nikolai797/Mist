4922851d76717a2cc4e3b82e0a628ee7991a9e64
Malikith <l.k.shishkov@gmail.com>
2026-02-13 02:55:55 +0200

    Add files via upload

---
+++ ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using GameStore.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace GameStore.Data.Identity;
+
+public class ApplicationUser : IdentityUser
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+
+    // Един потребител има много поръчки
+    public virtual ICollection<Order>? Orders { get; set; }
+}
