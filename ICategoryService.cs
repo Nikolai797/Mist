bbb6597d9ec90b7af11171d5ca2e91c3d71bf661
Malikith <l.k.shishkov@gmail.com>
2026-02-13 02:56:51 +0200

    Add files via upload

---
+++ ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using GameStore.Data.Entities;
+
+namespace GameStore.Services.Interfaces;
+
+public interface ICategoryService
+{
+    Task<IEnumerable<Category>> GetAllCategoriesAsync();
+    Task<Category?> GetCategoryByIdAsync(int id);
+    Task CreateCategoryAsync(Category category);
+    Task UpdateCategoryAsync(Category category);
+    Task DeleteCategoryAsync(int id);
+}
