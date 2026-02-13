69caddb956f9cd7a8e6597648c52da0a60d6bce0
Malikith <l.k.shishkov@gmail.com>
2026-02-13 02:55:08 +0200

    Add files via upload

---
+++ Order.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using GameStore.Data.Identity;   
+
+namespace GameStore.Data.Entities;
+
+public class Order
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public DateTime OrderDate { get; set; }
+
+    [Required]
+    public string UserId { get; set; } = null!;
+
+   
+    public virtual ApplicationUser? User { get; set; }
+
+    public virtual ICollection<OrderItem>? OrderItems { get; set; }
+}
