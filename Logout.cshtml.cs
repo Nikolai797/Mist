0d21a80f914a9a33fb6cf89b2d82a5cee706eed5
Malikith <l.k.shishkov@gmail.com>
2026-02-13 22:26:07 +0200

    Update Logout.cshtml.cs

--- Logout.cshtml.cs
+++ Logout.cshtml.cs
@@ -1,5 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
+
 #nullable disable
 
 using System;
@@ -15,19 +14,19 @@ namespace GAMEstore23.Areas.Identity.Pages.Account
 {
     public class LogoutModel : PageModel
     {
-        private readonly SignInManager<ApplicationUser> _signInManager;   // ← ApplicationUser
+        private readonly SignInManager<ApplicationUser> _signInManager;   
         private readonly ILogger<LogoutModel> _logger;
 
         public LogoutModel(SignInManager<ApplicationUser> signInManager, ILogger<LogoutModel> logger)
         {
             _signInManager = signInManager;
-            _logger = logger;        // ← ТУК НЕ ТРЯБВА ДА ПИШЕ jogger
+            _logger = logger;        
         }
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
-            _logger.LogInformation("User logged out.");   // ← И ТУК НЕ ТРЯБВА ДА ПИШЕ jogger
+            _logger.LogInformation("User logged out.");   
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
@@ -39,3 +38,4 @@ namespace GAMEstore23.Areas.Identity.Pages.Account
         }
     }
 }
+
