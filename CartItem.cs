f4a7293e218e80d345537f0953dbff960a042bc9
Malikith <l.k.shishkov@gmail.com>
2026-02-21 22:43:29 +0200

    Add files via upload

---
+++ CartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace GameStore.Services
+{
+    public class CartItem
+    {
+        public int GameId { get; set; }
+        public string GameTitle { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string UserId { get; set; }
+    }
+}
